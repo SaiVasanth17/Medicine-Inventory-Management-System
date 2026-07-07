92f75a99b577cab4e709414b0697619b9279d07e
Sai Vasanth <vasanthboggarapu@gmail.com>
2026-07-07 18:11:23 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+export default axios.create({
+  baseURL: "https://medicine-inventory-management-system.onrender.com/api",
 });
-
-export default api;
